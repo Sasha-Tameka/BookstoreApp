527c171ea52bad934dd496b35ce0f827425eb757
Sasha-Tameka <sashh92@hotmail.com>
2025-04-07 11:43:35 -0400

    save firebase config

--- obj/Debug/net9.0/BookstoreApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookstoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookstoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b698ede548dd98b73f56799b9da3150190fee527")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66c8ac4bf2def228f975cd9389e916a56c97377d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookstoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookstoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ wwwroot/js/firebase-auth.js
@@ -0,0 +1,54 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgfSDwaNHaqCUdqskOmbVcarg8tVK7kbY",
+  authDomain: "bookstoreapp-719b6.firebaseapp.com",
+  projectId: "bookstoreapp-719b6",
+  storageBucket: "bookstoreapp-719b6.firebasestorage.app",
+  messagingSenderId: "615824493852",
+  appId: "1:615824493852:web:ec2fe6dfed664b7c41fed9",
+  measurementId: "G-D2D9PM03L9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Signup Function
+document
+  .getElementById("signup-form")
+  ?.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      alert("Signup Successful! Please log in.");
+      window.location.href = "/Book/Index";
+    } catch (error) {
+      console.error("Signup Error:", error.message);
+      alert("Signup failed: " + error.message);
+    }
+  });
+// Login Function
+document.getElementById("login-form")?.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const email = e.target[0].value;
+  const password = e.target[1].value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    alert("Login Successful!");
+    window.location.href = "/Book/Index";
+  } catch (error) {
+    console.error("Login Error:", error.message);
+    alert("Login failed: " + error.message);
+  }
+});
