2c8413cc1bc8a1c64062e62a22676565b758e912
Sasha-Tameka <sashh92@hotmail.com>
2025-04-24 17:16:33 -0400

    styled edit page

--- obj/Debug/net9.0/BookstoreApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookstoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookstoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed4227bda613321809628f2a1cdd4b82b85a8ee2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fdc5bb1ff75da218a50de3bac3ce86b5e1621fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookstoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookstoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
